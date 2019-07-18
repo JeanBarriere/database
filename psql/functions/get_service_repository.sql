93f0cd8cf15148925f5832b3567f32f32d154323
Jude Pereira <judebpereira@gmail.com>
2019-07-18 17:27:24 +0200

    Allow exporting repo info for public services or services which the current user owns. (#82)

--- psql/functions/get_service_repository.sql
+++ psql/functions/get_service_repository.sql
@@ -9,8 +9,8 @@ CREATE FUNCTION app_public.get_service_repository(service_uuid uuid) RETURNS app
   FROM app_public.services s
   INNER JOIN app_hidden.repos r ON (r.uuid = s.repo_uuid)
   INNER JOIN app_public.owner_vcs o ON (o.uuid = r.owner_vcs_uuid)
-  WHERE s.uuid = service_uuid
+  WHERE s.uuid = service_uuid AND (s.public OR s.owner_uuid = current_owner_uuid())
   LIMIT 1
-$$ LANGUAGE sql STABLE SET search_path FROM CURRENT;
+$$ LANGUAGE sql STABLE SECURITY DEFINER SET search_path FROM CURRENT;
 
 COMMENT ON FUNCTION app_public.get_service_repository(uuid) IS E'Return the repository information for a single service.';
