12f8046b9d8342b5decab2b9f0ea97767d529210
Jean Barriere <jean@barriere.io>
2019-08-15 14:18:40 +0200

    feat(trigger): add trigger on teams INSERT to prevent owners with is_user=true to be owners of teams

--- psql/triggers/ensure_organization_owner_is_not_user.sql
+++ psql/triggers/ensure_organization_owner_is_not_user.sql
@@ -1,6 +1,4 @@
--- Trigger to abort if a team is inserted with a user-owner
-
-CREATE OR REPLACE FUNCTION ensure_organization_owner_is_not_user() RETURNS TRIGGER AS $$
+CREATE FUNCTION ensure_organization_owner_is_not_user() RETURNS TRIGGER AS $$
 BEGIN
 
     IF EXISTS (SELECT 1 FROM app_public.owners WHERE uuid = NEW.owner_uuid AND is_user = TRUE LIMIT 1) THEN
--- psql/triggers/main.sql
+++ psql/triggers/main.sql
@@ -2,3 +2,4 @@
 \ir must_have_one_admin.sql
 \ir create_app_default_dns.sql
 \ir owner_username_conflict.sql
+\ir ensure_organization_owner_is_not_user.sql
