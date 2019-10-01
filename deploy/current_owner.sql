b1f0ac9e334dbd7b078542d9882a67da8bb5f2f6
Jean Barriere <jean@barriere.io>
2019-10-01 16:16:05 +0200

    feat(current-owner): set search-path

--- deploy/current_owner.sql
+++ deploy/current_owner.sql
@@ -2,8 +2,10 @@
 
 BEGIN;
 
-CREATE FUNCTION current_owner() RETURNS app_public.owners AS $$
-SELECT * FROM app_public.owners WHERE owners.uuid = current_owner_uuid() LIMIT 1;
+SET search_path TO app_public,app_hidden,app_private,app_runtime,public;
+
+CREATE FUNCTION current_owner() RETURNS owners AS $$
+SELECT * FROM owners WHERE owners.uuid = current_owner_uuid() LIMIT 1;
 $$ LANGUAGE sql STABLE SET search_path FROM CURRENT;
 
 COMMIT;
