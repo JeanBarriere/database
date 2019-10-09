9f3a9e861f4a0b3cde1bb130fcc9cf13b78ee3bc
Jean Barriere <jean@barriere.io>
2019-10-09 17:23:39 +0200

    feat(service-name): change regex to match storyscript compiler regex

--- deploy/fix_alias_type.sql
+++ deploy/fix_alias_type.sql
@@ -5,11 +5,8 @@ BEGIN;
 -- drop old constraint
 ALTER DOMAIN app_public.alias DROP CONSTRAINT alias_check;
 
--- replace current services names containing dots with dashes
-UPDATE app_public.services SET name = regexp_replace(name, '\.', '-', '');
-
 -- add new constraint
 ALTER DOMAIN app_public.alias ADD CONSTRAINT alias_check CHECK ((length((VALUE)::text) > 1) AND (length((VALUE)::text) < 25) AND
-                                (VALUE ~ '^[\w]+[\w\-]*[\w]+$'::citext));
+                                (VALUE ~ '^[a-zA-Z][a-zA-Z-_0-9]*$'::citext));
 
 COMMIT;
