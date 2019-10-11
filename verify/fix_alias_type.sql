066003f5729e9e21e19812a219588e970a21b622
Jude Pereira <contact@judepereira.com>
2019-10-11 13:21:52 +0200

    Add assertions validating the "alias" type.

--- verify/fix_alias_type.sql
+++ verify/fix_alias_type.sql
@@ -4,14 +4,42 @@ SET search_path TO :search_path;
 
 BEGIN;
 
-DO $$
-BEGIN
+-- Inspired from https://dba.stackexchange.com/a/203937/161921
+create function try_cast(t text)
+    returns alias
+as
+$$
+begin
+    begin
+        return cast(t as alias);
+    exception
+        when others then
+            return null;
+    end;
+end;
+$$
+    language plpgsql;
 
--- checking that no services contain '.' in their names anymore
-ASSERT (SELECT count(*) FROM services WHERE name ~ '\.') = 0, 'cannot have services';
+do
+$$
+    begin
+        assert try_cast('helloworld') is not null;
+        assert try_cast('hello123') is not null;
+        assert try_cast('h2') is not null;
+        assert try_cast('hello_') is not null;
+        assert try_cast('world-') is not null;
+        assert try_cast('hello-world') is not null;
+        assert try_cast('hello_world') is not null;
 
-END;
+        assert try_cast('hello world') is null;
+        assert try_cast('123') is null;
+        assert try_cast('1world') is null;
+        assert try_cast('hello.world') is null;
+        assert try_cast('-') is null;
+        assert try_cast('_') is null;
+        assert try_cast('.') is null;
+        assert try_cast('.helloworld') is null;
+    end;
 $$;
-
 
 ROLLBACK;
