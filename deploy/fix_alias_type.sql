a51bb4dbabfa6e4db85bf2944ed6d984835848d4
Jude Pereira <contact@judepereira.com>
2019-10-11 13:21:24 +0200

    Escape the regex string correctly.

--- deploy/fix_alias_type.sql
+++ deploy/fix_alias_type.sql
@@ -7,6 +7,8 @@ ALTER DOMAIN app_public.alias DROP CONSTRAINT alias_check;
 
 -- add new constraint
 ALTER DOMAIN app_public.alias ADD CONSTRAINT alias_check CHECK ((length((VALUE)::text) > 1) AND (length((VALUE)::text) < 25) AND
-                                (VALUE ~ '^[a-zA-Z][a-zA-Z-_0-9]*$'::citext));
+                                (VALUE ~ '^[a-zA-Z][a-zA-Z\-_0-9]*$'::citext));
+
+
 
 COMMIT;
