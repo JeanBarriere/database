16ce5936e3a654d92036c2eeb7cec24d2465bcda
Jean Barriere <jean@barriere.io>
2020-02-13 12:23:07 +0100

    fix(types): dropping and setting back not null

--- revert/studio_specifications.sql
+++ revert/studio_specifications.sql
@@ -185,9 +185,11 @@ create table "app_runtime"."subscriptions" (
 
 ALTER TYPE release_state rename to release_state_old;
 CREATE TYPE release_state as enum('QUEUED', 'DEPLOYING', 'DEPLOYED', 'TERMINATING', 'TERMINATED', 'NO_DEPLOY', 'FAILED', 'SKIPPED_CONCURRENT', 'TIMED_OUT', 'TEMP_DEPLOYMENT_FAILURE');
+alter table "app_public"."releases" alter column "state" drop not null;
 alter table "app_public"."releases" alter column "state" drop default;
 alter table "app_public"."releases" alter column "state" type release_state USING "state"::text::release_state;
 alter table "app_public"."releases" alter column "state" set default 'QUEUED'::release_state;
+alter table "app_public"."releases" alter column "state" set not null;
 drop type release_state_old;
 
 -- -- alter type
